d144160d74245e81f44cdbe6c0479d988fa5e1b8
Yuhan Sun <ys2883@columbia.edu>
2016-02-23 18:14:25 -0500

    Added files via upload

--- doc/map/global.R
+++ doc/map/global.R
@@ -3,19 +3,7 @@
 library(dplyr)
 
 #load dataset
-request_short <- read.csv("~/Desktop/project2/311_Service_Requests_from_2010_to_Present.csv")
-request_short$Latitude=jitter(request_short$Latitude)
-request_short$Longitude=jitter(request_short$Longitude)
-
-request.clean=request_short%>%
-  select(
-    latitude=Latitude,
-    longitude=Longitude,
-    zipcode=Incident.Zip,
-    type=Complaint.Type
-  )
-request.clean=na.omit(request.clean)
-
+request.clean <- read.csv("/Users/sunxiaohan/Desktop/map/request.clean.csv")
 
 ####################word cloud
 ##################################
